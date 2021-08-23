8ca4ecb5c64c48438364394a831f358bda5b8b61
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-23 19:24:41 +0700

    Clean up code

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Webpack 5 boilerplate using Babel, PostCSS, Eslint, Prettier, React and Typescript with an optimized production build.
 
 ## Notes
+
+https://nerdcave.com/tailwind-cheat-sheet
+https://microvn.net/tailwind-cuoc-but-pha-ngoan-muc-tren-con-duong-su-dung-atomic-css.html
+https://viblo.asia/p/dong-dem-mot-so-cach-to-chuc-css-bWrZn7jblxw
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,9 +5,10 @@ import Container from '../../components/Container'
 const Home = () => {
   return (
     <Container>
-      <Card title="Thống kê" description="thống kê" />
-      <Card title="Thống kê 2" description="thống kê 2" />
-      <Card title="Thống kê 3" description="thống kê 3" />
+      <Card
+        title="Chào mừng bạn đến với bộ Webpack của Quốc Vũ"
+        description="Prettier, Eslint, TailwindCSS, StyledComponents"
+      />
     </Container>
   )
 }
