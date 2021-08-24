5bfe29b65f3703e3db4930377ec15a8c59e5f489
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-08-24 15:36:58 +0700

    Config env process

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,11 @@
 const webpack = require('webpack')
-
+const dotenv = require('dotenv')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Codevolution'),
+      'process.env': JSON.stringify(dotenv.config().parsed), // it will automatically pick up key values from .env file
     }),
   ],
 }
