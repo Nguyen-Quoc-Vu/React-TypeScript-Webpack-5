2ea265f836bb8870f3ea6d2f849ba827714852d9
Quốc Vũ <nguyenquocvu.work@gmail.com>
2021-09-27 17:36:04 +0700

    Update dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,7 @@
-module.exports = {
-  semi: false,
-  trailingComma: 'es5',
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 2,
-  endOfLine: 'auto',
-}
+export const printWidth = 80;
+export const tabWidth = 2;
+export const useTabs = false;
+export const semi = true;
+export const singleQuote = true;
+export const trailingComma = 'all';
+export const arrowParens = 'avoid';
